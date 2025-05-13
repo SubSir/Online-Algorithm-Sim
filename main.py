9e92b1410de5dc84a546dd87b5a201636e4b86ca
SubSir <tiancaizhangdiaxian@sjtu.edu.cn>
2025-05-13 23:49:55 +0800

    Last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 from sklearn.model_selection  import train_test_split
@@ -54,16 +55,147 @@ def main():
         os.remove(output_file) 
         open(output_file, "w").close()
  
+    algorithms = [alg.strip() for alg in args.algorithm.split(",")] 
+    total_results = {}
+    total_requsts = 0
+    total_uniquepages = 0
+    for alg in algorithms:
+        total_results[alg] = 0
+    
     for trace_file in traces_list:
         print(f"Processing {trace_file}")
         requests = input_processor.process_input(trace_file) 
 
         # 如果指定了多个算法，则依次运行
-        algorithms = [alg.strip() for alg in args.algorithm.split(",")] 
         results = {}
  
-        train_r, _ = train_test_split(requests, test_size=0.9, shuffle=False)
         test_r = requests
+        if args.task3:
+            belady = Belady(args.cache_size) 
+            belady.initial(test_r) 
+            belady.resize(len(test_r)) 
+
+            isvm = ISVM_Cache(args.cache_size, upper_bound=30, k=30)
+            # isvm.predict_online(train_r) 
+            predict = isvm.predict_online(test_r) 
+            # print(predict)
+            scheduler = LSTMScheduler(args.cache_size) 
+            result = scheduler.run(test_r,  predict)
+            results["No threshold"] = {
+                "total_requests": result.total_requests, 
+                "unique_pages": result.unique_pages, 
+                "cache_misses": result.cache_misses  
+            }
+
+            for i in range(10, 60, 10):
+                scheduler = LSTMScheduler(args.cache_size)
+                predicts = []
+                for j in predict:
+                    if j > i:
+                        predicts.append(1)
+                    elif j < 0:
+                        predicts.append(-1)
+                    else:
+                        predicts.append(0)
+                result = scheduler.run(test_r,  predicts)
+                results[i] = {
+                "total_requests": result.total_requests, 
+                "unique_pages": result.unique_pages, 
+                "cache_misses": result.cache_misses  
+            }
+            res = results[list(results.keys())[0]]
+            print(f"Total  number of requests: {res['total_requests']}")
+            print(f"Total  number of unique pages: {res['unique_pages']}")
+            for alg, res in results.items(): 
+                print(f"{alg}'s number of cache misses: {res['cache_misses']}")
+
+            # 收集x（不同阈值）和y（cache misses）
+            x = []
+            y = []
+
+            for key in results:
+                if key == "No threshold":
+                    continue  # 一会单独画
+                x.append(int(key))
+                y.append(results[key]["cache_misses"])
+
+            # 画不同阈值下的cache miss数量
+            plt.figure()
+            plt.plot(x, y, marker='o', label="Thresholded eviction")
+
+            # 画无阈值基线（即传统策略，作为比较）
+            plt.axhline(y=results["No threshold"]["cache_misses"], color="red", linestyle="--", label="No threshold (baseline)")
+
+            plt.xlabel("Eviction threshold")
+            plt.ylabel("Cache misses")
+            plt.title("Cache misses vs. eviction threshold")
+            plt.legend(loc='upper right')
+            plt.grid()
+            plt.savefig(f"{trace_file}_thresholdresult.png")
+            plt.close()
+            continue
+
+        if args.task2:
+            belady = Belady(args.cache_size) 
+            belady.initial(test_r) 
+            belady.resize(len(test_r)) 
+
+            isvm = ISVM_Cache(args.cache_size, upper_bound=30, k=30)
+            # isvm.predict_online(train_r) 
+            predict = isvm.predict_online(test_r) 
+            # print(predict)
+            scheduler = LSTMScheduler(args.cache_size) 
+            result = scheduler.run(test_r,  predict)
+            results["Infinity"] = {
+                "total_requests": result.total_requests, 
+                "unique_pages": result.unique_pages, 
+                "cache_misses": result.cache_misses  
+            }
+            for i in range(2,9):
+                isvm = ISVM_Cache2(args.cache_size, upper_bound=30, k=30, M=2**i)
+                # isvm.predict_online(train_r) 
+                predict = isvm.predict_online(test_r) 
+                # print(predict)
+                scheduler = LSTMScheduler(args.cache_size) 
+                result = scheduler.run(test_r,  predict)
+                results[str(i)] = {
+                    "total_requests": result.total_requests, 
+                    "unique_pages": result.unique_pages, 
+                    "cache_misses": result.cache_misses  
+                }
+            res = results[list(results.keys())[0]]
+            print(f"Total  number of requests: {res['total_requests']}")
+            print(f"Total  number of unique pages: {res['unique_pages']}")
+            for alg, res in results.items(): 
+                print(f"{alg}'s number of cache misses: {res['cache_misses']}")
+
+            # 收集x和y
+            x = []
+            y = []
+
+            for key in results:
+                if key == "Infinity":
+                    continue  # 一会单独画
+                x.append(int(key))
+                y.append(results[key]["cache_misses"])
+
+            # 画PC哈希不同bit位的cache miss数
+            plt.figure()
+            plt.plot(x, y, marker='o', label="PC hash (last i bits)")
+
+            # 画Belady或无限的基线
+            plt.axhline(y=results["Infinity"]["cache_misses"], color="red", linestyle="--", label="Infinity")
+
+            plt.xlabel("i (Hash with last i bits, i=log2(M))")
+            plt.ylabel("Cache misses")
+            plt.title("Cache misses vs PC Hash bits")
+            plt.legend(loc='upper right')
+            plt.grid()
+            plt.savefig(f"{trace_file}_hashresult.png")
+            plt.close()
+            continue
+
+
         for alg in algorithms:
             print(f"\nRunning algorithm: {alg}")
 
@@ -136,33 +268,35 @@ def main():
         print(f"Total  number of requests: {res['total_requests']}")
         print(f"Total  number of unique pages: {res['unique_pages']}")
 
-        # 准备画图数据
-        algs = list(results.keys())
-        cache_misses = [results[alg]['cache_misses'] for alg in algs]
-
-        # 画柱状图
-        plt.figure(figsize=(8,5))
-        plt.bar(algs, cache_misses, color='skyblue', width=0.5)
-        plt.xlabel('Algorithm')
-        plt.ylabel('Number of Cache Misses')
-        plt.title(f'Cache Misses Comparison ({res["total_requests"]} total requests, {res["unique_pages"]} unique pages)')
-        for i, miss in enumerate(cache_misses):
-            plt.text(i, miss+1, str(miss), ha='center', va='bottom')
-        plt.ylim(0, max(cache_misses)*1.2)
-        plt.tight_layout()
-        plt.savefig("cache_misses_bar.png")
+        total_requsts += res['total_requests']
+        total_uniquepages += res['unique_pages']
 
         for alg, res in results.items(): 
             print(f"{alg}'s number of cache misses: {res['cache_misses']}")
+            total_results[alg] += res['cache_misses']
 
-        with open(output_file, "a") as f:
-            f.write(f"Trace  file: {trace_file}\n")
-            for alg, res in results.items(): 
-                f.write(f"Algorithm:  {alg}\n")
-                f.write(f"Total  number of requests: {res['total_requests']}\n")
-                f.write(f"Total  number of unique pages: {res['unique_pages']}\n")
-                f.write(f"Total  number of cache misses: {res['cache_misses']}\n")
-            f.write("\n") 
+
+    print(f"Total  number of requests: {total_requsts}")
+    print(f"Total  number of unique pages: {total_uniquepages}")
+    # 准备画图数据
+    algs = list(total_results.keys())
+    cache_misses = [total_results[alg] for alg in algs]
+
+    # 画柱状图
+    plt.figure(figsize=(8,5))
+    plt.bar(algs, cache_misses, color='skyblue', width=0.5)
+    plt.xlabel('Algorithm')
+    plt.ylabel('Number of Cache Misses')
+    plt.title(f'Cache Misses Comparison ({total_requsts} total requests, {total_uniquepages} unique pages)')
+    for i, miss in enumerate(cache_misses):
+        plt.text(i, miss+1, str(miss), ha='center', va='bottom')
+    plt.ylim(0, max(cache_misses)*1.2)
+    plt.tight_layout()
+    plt.savefig("cache_misses_bar.png")
+
+    for alg, res in results.items(): 
+        print(f"{alg}'s number of cache misses: {total_results[alg]}")
+
  
 if __name__ == "__main__":
     main()
